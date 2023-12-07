61bba0f58de101682b6c5164fd2d6adde789d152
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-07 15:02:56 +0000

    refactor: simplify calculating the min and max in all cases

--- modules/min_max_profit.py
+++ modules/min_max_profit.py
@@ -1,10 +1,4 @@
 def min_max(value: list[int]):
-    if value == [1]:
-        return [min(value), max(value)]
-    if value == [2]:
-        return [min(value), max(value)]
-    if value == [1, 2, 3, 4, 5]:
-        return [min(value), max(value)]
-    if value == [2334454, 5]:
-        return [min(value), max(value)]
-    raise ValueError("Input must be a list")
+    if not isinstance(value, list):
+        raise ValueError("Input must be a list")
+    return [min(value), max(value)]
