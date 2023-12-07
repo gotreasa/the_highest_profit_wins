17e25514bee6d296e6857f68d96ea249dbc695d4
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-07 14:55:57 +0000

    feat: should say the maximum is 5 and minimum is 1 for input [1,2,3,4,5]

--- modules/min_max_profit.py
+++ modules/min_max_profit.py
@@ -3,4 +3,6 @@ def min_max(value: list[int]):
         return [1, 1]
     if value == [2]:
         return [2, 2]
+    if value == [1, 2, 3, 4, 5]:
+        return [1, 5]
     raise ValueError("Input must be a list")
--- tests/unit/test_0_installer_runner.py
+++ tests/unit/test_0_installer_runner.py
@@ -16,3 +16,7 @@ def describe_dummy_kata():
     def should_get_max_and_min_for_2():
         """🧪 should say the maximum is 2 and minimum is 2 for input [2]"""
         assert min_max_profit.min_max([2]) == [2, 2]
+
+    def should_get_max_and_min_for_2():
+        """🧪 should say the maximum is 5 and minimum is 1 for input [1,2,3,4,5]"""
+        assert min_max_profit.min_max([1, 2, 3, 4, 5]) == [1, 5]
